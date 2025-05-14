66a23e00692c3cb3e0453725dfadf8986c9423a1
Pouya Taghipour <87365616+Pouya-Ta@users.noreply.github.com>
2025-05-14 11:08:25 +0330

    Update Assign_Channel_Locations.m

--- DataPrepration/Assign_Channel_Locations.m
+++ DataPrepration/Assign_Channel_Locations.m
@@ -1,1 +1,14 @@
 function assign_channel_locations(set_file, channels_file, electrodes_file, output_file)
+    % ---------------------------------------------------------------------
+    % ASSIGN EEG CHANNEL LOCATIONS IN EEGLAB
+    % 
+    % Inputs:
+    %   - set_file: Path to the EEG .set file
+    %   - channels_file: Path to the channels.tsv file
+    %   - electrodes_file: Path to the electrodes.tsv file
+    %   - output_file: Name of the saved EEG file
+    %
+    % Outputs:
+    %   - Updated EEG file with correct channel locations
+    %
+    % ---------------------------------------------------------------------
