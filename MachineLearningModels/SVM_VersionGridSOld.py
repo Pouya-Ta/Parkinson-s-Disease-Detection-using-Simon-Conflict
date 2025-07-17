cd2f44ae3171fe0f8864dd0b025d4e1d980dc536
Pouya Taghipour <87365616+Pouya-Ta@users.noreply.github.com>
2025-07-17 17:55:59 +0330

    Update SVM_VersionGridSOld.py

--- MachineLearningModels/SVM_VersionGridSOld.py
+++ MachineLearningModels/SVM_VersionGridSOld.py
@@ -3,3 +3,15 @@
 from google.colab import drive
 drive.mount('/content/drive')
 """
+
+import numpy as np
+from sklearn.svm import SVC
+from sklearn.model_selection import StratifiedKFold, GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import classification_report, confusion_matrix
+
+# Load Data
+X = np.load("lstm_selected_features.npy")  # shape: (N, 32 or 64) | Optimize this path and adjust it based on your own path for selected features after the hybrid EEGNet-LSTM models
+y = np.load("lstm_labels.npy")             # shape: (N,) | Optimize this path and adjust it based on your own path for labels (as another input for the model)
+
