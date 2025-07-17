2350c5035a165c2cc69ca29def2a02ca537260c1
Pouya Taghipour <87365616+Pouya-Ta@users.noreply.github.com>
2025-07-17 17:41:50 +0330

    Update SVM.py

--- MachineLearningModels/SVM.py
+++ MachineLearningModels/SVM.py
@@ -1,5 +1,16 @@
-# If you are using colab as an environment you can uncomment the code below
+# If you are using Colab as an environment, you can uncomment the code below
 """
 from google.colab import drive
 drive.mount('/content/drive')
 """
+import numpy as np
+from sklearn.svm import SVC
+from sklearn.model_selection import StratifiedGroupKFold, GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import classification_report, ConfusionMatrixDisplay
+
+# === Load Data ===
+X = np.load("/content/drive/MyDrive/ParkinsonClassifier/lstm_selected_features.npy")  # shape: (N, 32 or 64)
+y = np.load("/content/drive/MyDrive/ParkinsonClassifier/lstm_labels.npy")             # shape: (N,)
+groups = np.load("/content/drive/MyDrive/ParkinsonClassifier/lstm_groups.npy")        # shape: (N,)
