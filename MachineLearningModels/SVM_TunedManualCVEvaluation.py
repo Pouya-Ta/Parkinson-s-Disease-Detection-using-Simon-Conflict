b49d8c84dfd0b683a29e21c3f577175554397ea1
Pouya Taghipour <87365616+Pouya-Ta@users.noreply.github.com>
2025-07-17 17:59:01 +0330

    Update SVM_TunedManualCVEvaluation.py

--- MachineLearningModels/SVM_TunedManualCVEvaluation.py
+++ MachineLearningModels/SVM_TunedManualCVEvaluation.py
@@ -1,4 +1,8 @@
-
+# If you are using Colab as an environment, you can uncomment the code below
+"""
+from google.colab import drive
+drive.mount('/content/drive')
+"""
 
 from sklearn.svm import SVC
 from sklearn.model_selection import StratifiedKFold
@@ -7,3 +11,4 @@ from sklearn.metrics import (
     accuracy_score, precision_score, recall_score, f1_score, classification_report, confusion_matrix
 )
 import numpy as np
+
