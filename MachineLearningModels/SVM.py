6475e715e4c184af59792b53a5ccb9490a0aea1e
Pouya Taghipour <87365616+Pouya-Ta@users.noreply.github.com>
2025-07-17 17:42:51 +0330

    Update SVM.py

--- MachineLearningModels/SVM.py
+++ MachineLearningModels/SVM.py
@@ -10,7 +10,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import classification_report, ConfusionMatrixDisplay
 
-# === Load Data ===
-X = np.load("/content/drive/MyDrive/ParkinsonClassifier/lstm_selected_features.npy")  # shape: (N, 32 or 64)
+# Load Data
+X = np.load("lstm_selected_features.npy")  # shape: (N, 32 or 64) | Optimize this path and adjust it based on your own path for selected features after the hybrid EEGNet-LSTM models
 y = np.load("/content/drive/MyDrive/ParkinsonClassifier/lstm_labels.npy")             # shape: (N,)
 groups = np.load("/content/drive/MyDrive/ParkinsonClassifier/lstm_groups.npy")        # shape: (N,)
