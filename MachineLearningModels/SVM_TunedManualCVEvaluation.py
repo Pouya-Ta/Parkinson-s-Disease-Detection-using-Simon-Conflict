48d71fb7cfdb9b95a276aa9411d44a73b80dcd35
Pouya Taghipour <87365616+Pouya-Ta@users.noreply.github.com>
2025-07-17 17:58:41 +0330

    Create SVM_TunedManualCVEvaluation.py

---
+++ MachineLearningModels/SVM_TunedManualCVEvaluation.py
@@ -0,0 +1,9 @@
+
+
+from sklearn.svm import SVC
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import (
+    accuracy_score, precision_score, recall_score, f1_score, classification_report, confusion_matrix
+)
+import numpy as np
